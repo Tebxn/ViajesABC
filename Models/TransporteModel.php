3bafd18d7220156c0986ecb0dd536616f2d84f0a
Sharon-MN <77652147+Sharon-MN@users.noreply.github.com>
2023-04-21 17:06:36 -0600

    Consultar transporte y actualizar funcional

--- Models/TransporteModel.php
+++ Models/TransporteModel.php
@@ -21,4 +21,47 @@ function ConsultarTransportesModel() {
 
 }
 
+function ConsultarTransporteModel($TRANSPORTE_ID) {
+
+    $conn = conectar();
+
+    $cursor = oci_new_cursor($conn);
+
+    $stmt = oci_parse($conn, "BEGIN ConsultarTransporte(:pTRANSPORTE_ID, :pCursor); END;");
+    oci_bind_by_name($stmt, ":pTRANSPORTE_ID", $TRANSPORTE_ID);
+    oci_bind_by_name($stmt, ":pCursor", $cursor, -1, OCI_B_CURSOR);
+    oci_execute($stmt);
+    oci_execute($cursor);
+
+
+    $transporte = oci_fetch_array($cursor, OCI_ASSOC);
+
+
+    oci_free_statement($stmt);
+    oci_free_statement($cursor);
+    oci_close($conn);
+
+    return $transporte;
+
+}
+
+function ActualizarTransporteModel($TRANSPORTE_ID, $NOMBRE, $EMAIL, $TELEFONO)
+
+{
+    $conn = conectar();
+    $stmt = oci_parse($conn, "BEGIN actualizar_transporte(:pTRANSPORTE_ID, :pNOMBRE, :pEMAIL, :pTELEFONO); END;");
+
+    oci_bind_by_name($stmt, ':pTRANSPORTE_ID', $TRANSPORTE_ID);
+    oci_bind_by_name($stmt, ':pNOMBRE', $NOMBRE, 255);
+    oci_bind_by_name($stmt, ':pEMAIL', $EMAIL, 255);
+    oci_bind_by_name($stmt, ':pTELEFONO', $TELEFONO, 255);
+
+    oci_execute($stmt);
+
+    oci_free_statement($stmt);
+    oci_close($conn);
+
+}
+
+
 ?>
